73f349f413d9ec9c51e4314263a4356c62f77dca
Frances <fran.h.hitchcock@gmail.com>
2023-07-31 16:47:27 +0100

    server.js updated

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors")
+const mongoose = require("mongoose")
+const bp = require("body-parser")
+
+require("dotenv").config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.use(cors())
+app.use(bp.json())
+
+app.get("/", (request, response) =>{
+    response.status(200).json("This is the root endpoint")
+})
+
+app.listen(PORT, () => console.log(`app is listening on port ${PORT}`))
