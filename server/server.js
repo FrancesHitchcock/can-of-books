1c9787d5c5f8659c20b38bb05caf71da73cb7210
Frances <fran.h.hitchcock@gmail.com>
2023-08-01 15:17:03 +0100

    server.js updated, models/book.js created as schema

--- server/server.js
+++ server/server.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const cors = require("cors")
-const mongoose = require("mongoose")
-const bp = require("body-parser")
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bp = require("body-parser");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
-app.use(cors())
-app.use(bp.json())
+const app = express();
 
-app.get("/", (request, response) =>{
-    response.status(200).json("This is the root endpoint")
-})
+app.use(cors());
+app.use(bp.json());
 
-app.listen(PORT, () => console.log(`app is listening on port ${PORT}`))
+const Book = require("/models/book");
+mongoose.connect(process.env.DATABASE_URL);
+
+app.get("/", (request, response) => {
+  response.status(200).json("This is the root endpoint");
+});
+
+app.get("/books", async (request, response) => {
+  const allBooks = await Book.find(request.query);
+  response.status(200).json(allBooks);
+});
+
+app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
