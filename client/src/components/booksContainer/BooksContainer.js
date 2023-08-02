3afbdbf8ae44d2ceb126bfdc492c132ef39c8e19
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-02 14:40:39 +0100

    BooksContainer.js updated

--- client/src/components/booksContainer/BooksContainer.js
+++ client/src/components/booksContainer/BooksContainer.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BooksContainer.css";
 
 export default function BooksContainer({ allBooks }) {
   const booksMarkup = allBooks.map((book) => {
-    return <div>{book.title}</div>;
+    return (
+      <div key={book._id}>
+        <h2>{book.title}</h2>
+        <p>{book.description}</p>
+        <input type="checkbox" id="read" /> <label htmlFor="read">Read</label>
+      </div>
+    );
   });
 
   return <div className="books-container">{booksMarkup}</div>;
