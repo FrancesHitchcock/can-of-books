4b8983e02fbd82ce27a9c134791df2c6de531f98
Frances <fran.h.hitchcock@gmail.com>
2023-08-02 16:46:16 +0100

    updated AddForm.js

--- client/src/components/addForm/AddForm.js
+++ client/src/components/addForm/AddForm.js
@@ -2,6 +2,16 @@ import { useState } from "react";
 import "./AddForm.css";
 
 export default function AddForm() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  function handleChange(e) {
+    // console.log(e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    // console.log(formData);
+  }
+
   return (
     <form>
       <h1>Form</h1>
@@ -9,12 +19,14 @@ export default function AddForm() {
         type="text"
         placeholder="Title"
         name="title"
+        value={formData.title}
         onChange={handleChange}
       />
       <input
         type="text"
         placeholder="Description"
         name="description"
+        value={formData.description}
         onChange={handleChange}
       />
     </form>
