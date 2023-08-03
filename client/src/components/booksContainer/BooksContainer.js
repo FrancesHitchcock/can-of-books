bf179224470d1dec652a27b3a054b4685a52ba35
Frances <fran.h.hitchcock@gmail.com>
2023-08-03 15:50:07 +0100

    Book component added

--- client/src/components/booksContainer/BooksContainer.js
+++ client/src/components/booksContainer/BooksContainer.js
@@ -1,28 +1,10 @@
-import React, { useState } from "react";
 import "./BooksContainer.css";
 
-import UpdateForm from "../updateForm/UpdateForm";
+import Book from "../book/Book";
 
 export default function BooksContainer({ allBooks, handleDelete }) {
-  const [updateVisible, setUpdateVisible] = useState(false);
-
-  function showUpdateForm() {
-    setUpdateVisible(updateVisible ? false : true);
-  }
-
   const booksMarkup = allBooks.map((book) => {
-    return (
-      <div key={book._id}>
-        <div className="delete-btn">
-          <button onClick={() => handleDelete(book._id)}>X</button>
-        </div>
-        <h2>{book.title}</h2>
-        <p>{book.description}</p>
-        <input type="checkbox" id="read" /> <label htmlFor="read">Read</label>
-        <button onClick={showUpdateForm}>Edit book</button>
-        {updateVisible && <UpdateForm />}
-      </div>
-    );
+    return <Book key={book._id} book={book} handleDelete={handleDelete} />;
   });
 
   return <div className="books-container">{booksMarkup}</div>;
