5f463f4204fac132df558049717cc18dd9e241c1
Chris Palmer <christopherpalmer283@gmail.com>
2023-08-03 13:01:45 +0100

    header updated navigation buttons added

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,21 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
+import About from "./components/About/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,9 +1,21 @@
 import "./Header.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
-      <h1>Can of Books</h1>
+      <div>
+        <Link to="/about">
+          <button type="button">About</button>
+        </Link>
+      </div>
+
+      <Link to="/">
+        <div className="Sign">
+          <h1>Can of Books</h1>
+        </div>
+      </Link>
     </header>
   );
 }
