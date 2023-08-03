fcd0e045176d8b374aeb5a2e8671294e36fc650c
Frances <fran.h.hitchcock@gmail.com>
2023-08-03 16:49:18 +0100

    book update functionality added

--- client/src/components/book/Book.js
+++ client/src/components/book/Book.js
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 
 import "./Book.css";
 import UpdateForm from "../updateForm/UpdateForm";
+import axios from "axios";
 
 export default function Book({ book, handleDelete }) {
   const [updateVisible, setUpdateVisible] = useState(false);
 
+  async function handleUpdateBook(e, id, book) {
+    e.preventDefault();
+    await axios.put(`http://localhost:8080/books/${id}`, book);
+  }
+
   function showUpdateForm() {
     setUpdateVisible(updateVisible ? false : true);
   }
+
   return (
-    <div>
+    <div className="book-box">
       <div className="delete-btn">
         <button onClick={() => handleDelete(book._id)}>X</button>
       </div>
@@ -18,7 +25,9 @@ export default function Book({ book, handleDelete }) {
       <p>{book.description}</p>
       <input type="checkbox" id="read" /> <label htmlFor="read">Read</label>
       <button onClick={showUpdateForm}>Edit book</button>
-      {updateVisible && <UpdateForm />}
+      {updateVisible && (
+        <UpdateForm handleUpdateBook={handleUpdateBook} id={book._id} />
+      )}
     </div>
   );
 }
--- client/src/components/updateForm/UpdateForm.js
+++ client/src/components/updateForm/UpdateForm.js
@@ -1,22 +1,32 @@
 import "./UpdateForm.css";
+import { useState } from "react";
 
-export default function UpdateForm() {
+export default function UpdateForm({ handleUpdateBook, id }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  function handleUpdateFormChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   return (
-    <form>
+    <form onSubmit={(e) => handleUpdateBook(e, id, formData)}>
       <h1>Form</h1>
       <input
         type="text"
         placeholder="Title"
         name="title"
-        // value={formData.title}
-        // onChange={handleChange}
+        value={formData.title}
+        onChange={handleUpdateFormChange}
       />
       <input
         type="text"
         placeholder="Description"
         name="description"
-        // value={formData.description}
-        // onChange={handleChange}
+        value={formData.description}
+        onChange={handleUpdateFormChange}
       />
       <button>Update book</button>
     </form>
